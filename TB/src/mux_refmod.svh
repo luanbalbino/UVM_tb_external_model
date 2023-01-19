87fb34b14133d819d81f143e870227fbe305169f
Luan Balbino <luan.balbino@ee.ufcg.edu.br>
2023-01-19 11:23:30 -0300

    update phases in mux_refmod

--- TB/src/mux_refmod.svh
+++ TB/src/mux_refmod.svh
@@ -3,6 +3,8 @@ import "DPI-C" context function int my_mux(int a, int b, int c, int d, int sel);
 class mux_refmod extends uvm_component;
     `uvm_component_utils(mux_refmod)
 
+    event begin_refmodtask;
+
     // Transações de entrada e saída do bloco (entrada: vinda do (monitor|scoreboard) / saída: indo para o comparador).
     mux_transaction tr_in;
     mux_transaction tr_out;
@@ -20,10 +22,33 @@ class mux_refmod extends uvm_component;
 
     virtual function void build_phase(uvm_phase phase);
         super.build_phase(phase);
-        tr_out = mux_transaction::type_id::create("tr_out", this);
+       // tr_out = mux_transaction::type_id::create("tr_out", this);
     endfunction: build_phase
-    
+
+    // to call some features in parallel, if needed
+    virtual task run_phase(uvm_phase phase);
+        super.run_phase(phase);
+        fork
+            refmod_task();
+        join
+    endtask: run_phase
+
+    task refmod_task();
+        forever begin
+            @begin_refmodtask;
+            tr_out = mux_transaction::type_id::create("tr_out", this);
+            tr_out.result = my_mux(tr_in.a, tr_in.b, tr_in.c, tr_in.d, tr_in.sel);
+            #10;
+            out.write(tr_out);
+        end
+    endtask : refmod_task
+
+    virtual function write (mux_transaction t);
+        tr_in = mux_transaction::type_id::create("tr_in", this);
+        tr_in.copy(t);
+       -> begin_refmodtask;
+    endfunction: write
 
 
-endclass
+endclass: mux_refmod
 
