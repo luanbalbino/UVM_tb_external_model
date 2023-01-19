be8e3992e3a9ccb38173f11b7b0dc0b2c181152e
Luan Balbino <luan.balbino@ee.ufcg.edu.br>
2023-01-19 11:02:23 -0300

    add analysis ports, imp ports and build_phase on class

--- TB/src/mux_refmod.svh
+++ TB/src/mux_refmod.svh
@@ -7,7 +7,22 @@ class mux_refmod extends uvm_component;
     mux_transaction tr_in;
     mux_transaction tr_out;
 
+    // Porta imp para receber os dados vindos do scoreboard (necessario implementar função write)
+    uvm_analysis_imp #(mux_transaction, mux_refmod) in;
+    // porta para enviar os dados resultantes para fora da classe (um comparador dentro do scoreboard, inicialmente.)
+    uvm_analysis_port #(mux_transaction) out;
 
+    function new(string name = "mux_refmod", uvm_component parent);
+        super.new(name, parent);
+        in = new("in", this);
+        out = new("out", this);
+    endfunction : new
+
+    virtual function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        tr_out = mux_transaction::type_id::create("tr_out", this);
+    endfunction: build_phase
+    
 
 
 endclass
