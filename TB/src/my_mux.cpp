62992ed74025a027dfbd89cee7d8b6a6e77c4428
Luan Balbino <luan.balbino@ee.ufcg.edu.br>
2023-02-02 10:06:23 -0300

    add c struct if output is used

--- TB/src/my_mux.cpp
+++ TB/src/my_mux.cpp
@@ -3,7 +3,6 @@
 
 extern "C" int my_mux(i_mux_struct *obj){
   int y = 0;
-
   if(obj->en) {  
     switch(obj->sel){
       case 0: y = obj->a; break;
@@ -14,3 +13,17 @@ extern "C" int my_mux(i_mux_struct *obj){
   }
   return y;
 }
+
+// if output struct included
+
+//extern "C" int my_mux(i_mux_struct *obj, o_mux_struct *out){
+//  if(obj->en) {  
+//    switch(obj->sel){
+//      case 0: out->y = obj->a; break;
+//      case 1: out->y = obj->b; break;
+//      case 2: out->y = obj->c; break;
+//      case 3: out->y = obj->d; break;
+//    }   
+//  }
+//  else out->y = 0;
+//}
