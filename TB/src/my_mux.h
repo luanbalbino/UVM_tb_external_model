60ea975002f0eecd8eeaba8e8383b18f6711ff93
Luan Balbino <luan.balbino@ee.ufcg.edu.br>
2023-02-02 10:06:34 -0300

    add output struct

--- TB/src/my_mux.h
+++ TB/src/my_mux.h
@@ -6,3 +6,8 @@ typedef struct i_mux_struct {
     int sel;
     int en;
 } i_mux_struct;
+
+// this can be used to address output variables, i'll not include because the output for this module is a single variable.
+typedef struct o_mux_struct {
+    int y;
+} o_mux_struct;
