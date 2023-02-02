862a11318b7aab9bd171c737965977fc6832dba5
Luan Balbino <luan.balbino@ee.ufcg.edu.br>
2023-02-02 09:25:45 -0300

    updating my_mux.cpp

--- TB/src/my_mux.cpp
+++ TB/src/my_mux.cpp
@@ -1,23 +1,16 @@
 #include <stdio.h>
+#include "my_mux.h"
 
-extern "C" int my_mux(int a, int b, int c, int d,  int sel, int en){
+extern "C" int my_mux(i_mux_struct *obj){
   int y = 0;
 
-  if(en) {  
-    switch(sel){
-      case 0: y = a; break;
-      case 1: y = b; break;
-      case 2: y = c; break;
-      case 3: y = d; break;
+  if(obj->en) {  
+    switch(obj->sel){
+      case 0: y = obj->a; break;
+      case 1: y = obj->b; break;
+      case 2: y = obj->c; break;
+      case 3: y = obj->d; break;
     }   
   }
   return y;
 }
-
-
-// main(){
-//  int a = 2,b= 3,c = 4,d = 5,sel= 1,en =1;
-
-//  printf("asadsda %d\n", my_mux(a,b,c,d,sel,en));
-
-// }
