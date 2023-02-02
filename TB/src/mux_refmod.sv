b28affad3c06a7b967c7f54e9ebbdfab60ebdf68
Luan Balbino <luan.balbino@ee.ufcg.edu.br>
2023-02-02 10:05:59 -0300

    add output struct

--- TB/src/mux_refmod.sv
+++ TB/src/mux_refmod.sv
@@ -1,3 +1,4 @@
+// input struct of function
 typedef struct {
     int a;
     int b;
@@ -6,6 +7,11 @@ typedef struct {
     int sel;
     int en;
 } i_mux_struct;
+
+// output struct of function
+typedef struct {
+    int y;
+} o_mux_struct;
 
 import "DPI-C" context function int my_mux(input i_mux_struct);
 
@@ -17,6 +23,7 @@ class mux_refmod extends uvm_component;
     mux_transaction transaction_refmod;
 
     i_mux_struct i_mux;  
+    //o_mux_struct o_mux;
 
     uvm_analysis_imp #(mux_transaction, mux_refmod) refmod_in;
     uvm_analysis_port #(mux_transaction) refmod_out;
@@ -39,7 +46,7 @@ class mux_refmod extends uvm_component;
         join
     endtask: run_phase
 
-    task copy_task();
+    task copy_input_task();
             // struct variables
             i_mux.a = transaction_refmod.a;
             i_mux.b = transaction_refmod.b;
@@ -47,15 +54,15 @@ class mux_refmod extends uvm_component;
             i_mux.d = transaction_refmod.d;
             i_mux.en = transaction_refmod.en;
             i_mux.sel = transaction_refmod.sel;
-    endtask : copy_task
+    endtask : copy_input_task
 
 
     task refmod_task();
         forever begin
             @(begin_refmodtask);
-            copy_task();           
+            copy_input_task();           
             transaction_refmod.y = my_mux(i_mux);
-            `uvm_info("MUX.cpp", $sformatf("saida do modelo: %0d", transaction_refmod.y), UVM_LOW)
+            `uvm_info("MUX.cpp", $sformatf("saida do modelo: %0d", transaction_refmod.y), UVM_LOW);
             refmod_out.write(transaction_refmod);
         end
     endtask : refmod_task
